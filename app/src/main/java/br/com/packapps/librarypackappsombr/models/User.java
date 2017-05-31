76960c48247a3e9a221c1538409f2e7aa1acb55d
Paulo Linhares <paulo.linhares@yetgo.com.br>
2017-05-31 16:28:21 -0300

    created user model

---
+++ app/src/main/java/br/com/packapps/librarypackappsombr/models/User.java
@@ -0,0 +1,26 @@
+package br.com.packapps.librarypackappsombr.models;
+
+/**
+ * Created by paulolinhares on 31/05/17.
+ */
+
+public class User {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
